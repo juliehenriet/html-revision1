1aa93d3e91d2532f2840ecf404f9d24eb4c49c80
julie henriet <juliehenriet@gmail.com>
2018-10-15 15:08:05 +0200

    Create index.html

--- js/js.js
+++ js/js.js
@@ -1,1 +1,15 @@
 "use strict"
+
+$(document).ready(function(){
+
+ $('.nav-item');
+
+// on écoute le click
+
+$('.nav-item').click(function(){
+  console.log(this);
+  $(this).addClass('active');
+  $('.nav-item').removeClass('active');
+});
+
+});
